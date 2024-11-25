c3c86724c0eddfb7accbd4cd601814d9c829e498
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2024-11-25 16:35:06 +0200

    Add files via upload

---
+++ ex2_hw/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.Write("vvedit znachennya: ");
+            double value = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("vvedit vidsotok: ");
+            double percentage = Convert.ToDouble(Console.ReadLine());
+
+            double result = (value * percentage) / 100;
+
+            Console.WriteLine($"{percentage} % vid {value} = {result}");
+        }
+    }
+}
---
+++ ex3_hw/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("vvedit 4 cifri cherez probel: ");
+            string input = Console.ReadLine();
+
+
+            string[] digits = input.Split(' ');
+
+            if (digits.Length == 4)
+            {
+
+                string result = string.Join("", digits);
+
+                int number = int.Parse(result);
+
+                Console.WriteLine("vashe chislo: " + number);
+            }
+            else
+            {
+                Console.WriteLine("vvod onle 4 cifri.");
+            }
+
+        }
+    }
+}

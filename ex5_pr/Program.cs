712089947db9762ebb4c8552919fac8ea5efc2c9
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2024-11-25 16:32:57 +0200

    Add files via upload

---
+++ ex5_pr/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введіть число A (A < B):");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть число B (A < B):");
+            int B = int.Parse(Console.ReadLine());
+
+            if (A >= B)
+            {
+                Console.WriteLine("Число A повинно бути меншим за число B.");
+                return;
+            }
+
+            for (int i = A; i <= B; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
---
+++ ex6_pr/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введіть довжину лінії:");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть символ заповнювач:");
+            char fillCharacter = Console.ReadLine()[0];
+
+            Console.WriteLine("Введіть напрямок лінії (горизонтальна/вертикальна):");
+            string direction = Console.ReadLine().ToLower();
+
+            if (direction == "горизонтальна")
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write(fillCharacter);
+                }
+                Console.WriteLine(); 
+            }
+            else if (direction == "вертикальна")
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.WriteLine(fillCharacter);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Невідомий напрямок. Виберіть 'горизонтальна' або 'вертикальна'.");
+            }
+        }
+    }
+}
